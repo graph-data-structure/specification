9ad1d5e72a701e0fcc425e678072e86992dae567
aureooms <aurelien.ooms@gmail.com>
2015-08-17 10:52:19 +0000

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -216,6 +216,26 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "foldPrefix": "^",
               "name": "JavaScript"
             },
+            "sourcePath": "/home/aureooms/dev/js/js-graph-spec/js/src/001-spec/00-methods.js",
+            "projectPath": "js/src/001-spec/00-methods.js",
+            "targetPath": "001-spec/00-methods",
+            "pageTitle": "001-spec/00-methods",
+            "title": "00-methods"
+          },
+          "depth": 2,
+          "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
             "sourcePath": "/home/aureooms/dev/js/js-graph-spec/js/src/001-spec/01-Graph.js",
             "projectPath": "js/src/001-spec/01-Graph.js",
             "targetPath": "001-spec/01-Graph",
